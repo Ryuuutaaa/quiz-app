f6e967e68e248d7ffa9bef8d280e0133a89a5ea8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-26 23:52:59 +0700

    Add constants for color and padding in utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF8B94BC);
+const kGreenColor = Color(0xFF34C759);
+const kRedColor = Color(0xFFE33A3A);
+const kBlackColor = Color(0xFF000000);
+const kGrayColor = Color(0xFF8F8F8F);
+const kPrimaryGradient = LinearGradient(
+  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+const double kDefaultPadding = 20.0;
