a8437d8162a00f50b4714e12576afa1f78d7e003
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-27 00:38:10 +0700

    Disable debug banner and enhance layout in WelcomeScreen with padding and constants

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_application/utils/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -6,28 +7,32 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            "Lets Play Quiz",
-            style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text("Enter your Informastion below"),
-          const Spacer(),
-          TextField(
-            decoration: const InputDecoration(
-              filled: true,
-              fillColor: Color(0XFF1C2341),
-              hintText: "Full Name plase",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Lets Play Quiz",
+              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+            const Text("Enter your Informastion below"),
+            const Spacer(),
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Color(0XFF1C2341),
+                hintText: "Full Name plase",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
