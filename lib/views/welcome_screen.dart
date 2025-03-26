028eabe3f876511b960fa2a97be4016c035bd5b0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-27 01:00:27 +0700

    Refactor WelcomeScreen layout and fix typos in text and hint

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -9,28 +9,30 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
         children: [
-          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+          SvgPicture.asset("assets/bg.svg", fit: BoxFit.fitWidth),
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  const Spacer(flex: 2),
                   Text(
-                    "Lets Play Quiz",
+                    "Let's Play Quiz",
                     style: Theme.of(context).textTheme.headlineMedium!.copyWith(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const Text("Enter your Informastion below"),
+                  const Text("Enter your information below"),
                   const Spacer(),
                   TextField(
                     decoration: const InputDecoration(
                       filled: true,
-                      fillColor: Color(0XFF1C2341),
-                      hintText: "Full Name plase",
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name please",
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(12)),
                       ),
